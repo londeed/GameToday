36d92ce3de5c3822deb2a46e8e13227d2302fb7e
Christian <Christiancap2000@gmail.com>
2022-01-03 16:28:03 +0100

    Collegamento DB

---
+++ src/main/java/Model/Connessione/ConPool.java
@@ -0,0 +1,30 @@
+package Model.Connessione;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
+public class ConPool {
+    private static DataSource datasource;
+
+    public static Connection getConnection() throws SQLException {
+        if (datasource == null) {
+            PoolProperties p = new PoolProperties();
+            p.setUrl("jdbc:mysql://localhost:3306/gametoday?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            p.setUsername("root");
+            p.setPassword("Chriscap-00");
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMinIdle(10);
+            p.setRemoveAbandonedTimeout(60);
+            p.setRemoveAbandoned(true);
+            datasource = new DataSource();
+            datasource.setPoolProperties(p);
+        }
+        return datasource.getConnection();
+    }
+}
